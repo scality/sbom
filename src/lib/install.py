2daa521acdb854dedeb4fd002d6d66a8e9ac353d
Yoan Moscatelli <yoan.moscatelli@scality.con>
2024-08-30 08:33:01 +0000

    :construction_worker: add dependabot

--- src/lib/install.py
+++ src/lib/install.py
@@ -7,7 +7,7 @@ import requests
 from pyunpack import Archive
 
 # Define the scanners and their versions
-scanners = {"syft": "1.8.0", "grype": "0.79.1", "trivy": "0.53.0"}
+scanners = {"syft": "1.11.1", "grype": "0.80.0", "trivy": "0.54.1"}
 
 # Define the base URLs for the scanners
 ANCHORE_BASE_URL = (
